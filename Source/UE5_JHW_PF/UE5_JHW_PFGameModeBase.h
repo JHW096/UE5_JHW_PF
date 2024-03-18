cb784bc946b2b80f16a843f88907c169229e6e2b
Jeon <dbs_dmswl92@naver.com>
2024-03-18 18:49:52 +0900

    Add file_GitIgnore Unreal templates

---
+++ Source/UE5_JHW_PF/UE5_JHW_PFGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "UE5_JHW_PFGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UE5_JHW_PF_API AUE5_JHW_PFGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
