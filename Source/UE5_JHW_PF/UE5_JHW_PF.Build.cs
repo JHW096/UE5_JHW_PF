66b4ac3e606f0641cfb1b5f3bccdfb43876c4169
Jeon <dbs_dmswl92@naver.com>
2024-03-18 19:19:53 +0900

    Add Module, TestLevel, Set Start Level

--- Source/UE5_JHW_PF/UE5_JHW_PF.Build.cs
+++ Source/UE5_JHW_PF/UE5_JHW_PF.Build.cs
@@ -8,7 +8,7 @@ public class UE5_JHW_PF : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "HeadMountedDisplay", "NavigationSystem", "AIModule", "Niagara", "EnhancedInput", "GameplayTasks" });
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
